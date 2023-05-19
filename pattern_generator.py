c0cc00e75487027dee0ae8c1c11b8c554b52811a
Huaidian Daniel Hou <50754057+DanielHou315@users.noreply.github.com>
2023-05-19 09:31:24 +0800

    Update pattern_generator.py

--- pattern_generator.py
+++ pattern_generator.py
@@ -1,8 +1,15 @@
 import numpy as np
 import math
+
 '''Some Math Involved in generating patterns'''
 
 def generate_sphere(s, h, height_coeff):
+    '''
+    Generates Part of a Sphere with Height h and cross-section radius s. 
+    
+    height_coeff adjusts the height of the sphere cutout once the calculations are done,
+    shape post-adjustment looks like a paraboloid.
+    '''
     r = (s**2 - 4*h**2) / 8 / h
     array = np.zeros([s,s])
     center = [s/2, s/2]
