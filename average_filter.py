01607a7a7c63ea849335fe3c72422315b716f290
Huaidian Daniel Hou <50754057+DanielHou315@users.noreply.github.com>
2023-05-19 09:28:59 +0800

    Update average_filter.py

--- average_filter.py
+++ average_filter.py
@@ -2,6 +2,14 @@ import numpy as np
 import math
 
 def find_max_of_average(image, w, h, channels, i):
+    '''
+    Given an image of width w and height h, 
+    find Max Value for each channel in channels 
+    within each i*i square.
+    
+    Really slow at the moment with native python, 
+    trying to refactor to numpy at some point.
+    '''
     print("Processing Pixel ", w, h)
     # Aggregate Image Colors
     sum = []
